df283aa78e3ef88298bd871ea30338ce21c4e3a8
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-04-29 03:22:07 +0900

    feat: 공통 에러 응답 포맷

--- src/main/java/org/example/infratest/global/ErrorCode.java
+++ src/main/java/org/example/infratest/global/ErrorCode.java
@@ -3,6 +3,7 @@ package org.example.infratest.global;
 import org.springframework.http.HttpStatus;
 
 public interface ErrorCode {
+    String name();
     HttpStatus getHttpStatus();
     String getMessage();
 }
