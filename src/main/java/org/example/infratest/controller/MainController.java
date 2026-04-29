8ca427559512b7fbe14c40e12bd7449b64bf7f84
Developer-Choi-Jae-Young <77819208+Developer-Choi-Jae-Young@users.noreply.github.com>
2026-04-29 23:31:42 +0900

    Feat: 파일 업로드 API 엔드포인트 추가

--- src/main/java/org/example/infratest/controller/MainController.java
+++ src/main/java/org/example/infratest/controller/MainController.java
@@ -2,7 +2,7 @@ package org.example.infratest.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.infratest.service.FileUploadService;
-import org.example.infratest.service.MemberService;
+import org.example.infratest.service.impl.MemberServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,17 +15,17 @@ import java.io.IOException;
 @RequiredArgsConstructor
 @RequestMapping("/main")
 public class MainController {
-    private final MemberService memberService;
+    private final MemberServiceImpl memberServiceImpl;
     private final FileUploadService fileUploadService;
 
     @PostMapping("/save")
     public ResponseEntity<?> save(@RequestBody String name) {
-        return ResponseEntity.ok(memberService.save(name));
+        return ResponseEntity.ok(memberServiceImpl.save(name));
     }
 
     @GetMapping("/list")
     public ResponseEntity<?> list() {
-        return ResponseEntity.ok(memberService.list());
+        return ResponseEntity.ok(memberServiceImpl.list());
     }
 
     @PostMapping(path = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
